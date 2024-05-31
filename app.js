0b87c3943ae4931af454caecebe2eaddf886de8f
andraryandra <andraryandra38@gmail.com>
2024-05-31 23:57:24 +0700

    Feat: Struktur Beginner Express Node JS

---
+++ app.js
@@ -0,0 +1,35 @@
+require("dotenv").config({ path: `${process.cwd()}/.env` });
+const express = require("express");
+
+const authRouter = require("./routes/authRoute");
+const projectRouter = require("./routes/projectRoute");
+const userRouter = require("./routes/userRoute");
+
+const catchAsync = require('./utils/catchAsync');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require("./controller/errorController");
+
+const app = express();
+
+app.use(express.json());
+
+// all routes
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/projects", projectRouter);
+app.use("/api/v1/users", userRouter);
+
+
+app.use(
+  '*',
+  catchAsync(async (req, res, next) => {
+      throw new AppError(`Can't find ${req.originalUrl} on this server`, 404);
+  })
+);
+
+app.use(globalErrorHandler);
+
+const PORT = process.env.APP_PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log('Server up and running', PORT);
+});
---
+++ controller/projectController.js
@@ -0,0 +1,97 @@
+const project = require("../db/models/project");
+const user = require("../db/models/user");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+const { success } = require("../utils/response");
+
+const createProject = catchAsync(async (req, res, next) => {
+  const body = req.body;
+  const userId = req.user.id;
+  const newProject = await project.create({
+    title: body.title,
+    productImage: body.productImage,
+    price: body.price,
+    shortDescription: body.shortDescription,
+    description: body.description,
+    productUrl: body.productUrl,
+    category: body.category,
+    tags: body.tags,
+    createdBy: userId,
+  });
+
+  if (!newProject) {
+    return next(new AppError("Failed to create the project", 400));
+  }
+
+  return success(res, newProject, "Project created");
+});
+
+const getAllProject = catchAsync(async (req, res, next) => {
+  const userId = req.user.id;
+  const result = await project.findAll({
+    include: user,
+    where: { createdBy: userId },
+  });
+
+  if (!result) {
+    return next(new AppError("Data not found", 404));
+  }
+
+  return success(res, result, "Project found");
+});
+
+const getProjectById = catchAsync(async (req, res, next) => {
+  const projectId = req.params.id;
+  const result = await project.findByPk(projectId, {
+    include: user,
+  });
+
+  if (!result) {
+    return next(new AppError("Project ID not found", 404));
+  }
+
+  return success(res, result, "Project found");
+});
+
+const updateProject = catchAsync(async (req, res, next) => {
+  const userId = req.user.id;
+  const projectId = req.params.id;
+  const body = req.body;
+
+  const result = await project.findOne({
+    where: { id: projectId, createdBy: userId },
+  });
+
+  if (!result) {
+    return next(new AppError("Invalid project id", 400));
+  }
+
+  const updateResult = await result.update(body);
+
+  return success(res, updateResult, "Project updated");
+});
+
+const deleteProject = catchAsync(async (req, res, next) => {
+  const userId = req.user.id;
+  const projectId = req.params.id;
+
+  const result = await project.findOne({
+    where: { id: projectId, createdBy: userId },
+  });
+
+  if (!result) {
+    return next(new AppError("Invalid project id", 400));
+  }
+
+  await result.destroy();
+
+  return success(res, null, "Data Successfully Deleted");
+});
+
+module.exports = {
+  createProject,
+  getAllProject,
+  getProjectById,
+  updateProject,
+  deleteProject,
+};
